262a9d5d859b944f60f729d0ffaf21cd1e7a3987
jaecheon <jaecheon0222@gmail.com>
2022-03-18 15:52:28 +0900

    #2 로그인 UI 구성  - 추후 소셜 로그인 이미지 추가

---
+++ app/src/main/java/com/jc/promise_keeper/activities/sign_in_out/SignUpActivity.kt
@@ -0,0 +1,16 @@
+package com.jc.promise_keeper.activities.sign_in_out
+
+import com.jc.promise_keeper.R
+import com.jc.promise_keeper.common.UtilityBase
+import com.jc.promise_keeper.databinding.ActivitySignUpBinding
+
+class SignUpActivity : UtilityBase.BaseAppCompatActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+
+
+    override fun ActivitySignUpBinding.onCreate() {
+
+
+
+    }
+
+}
--- app/src/main/java/com/jc/promise_keeper/activities/splash/SplashActivity.kt
+++ app/src/main/java/com/jc/promise_keeper/activities/splash/SplashActivity.kt
@@ -1,14 +1,14 @@
 package com.jc.promise_keeper.activities.splash
 
-import com.jc.promise_keeper.MainActivity
 import com.jc.promise_keeper.R
+import com.jc.promise_keeper.activities.sign_in_out.SignUpActivity
 import com.jc.promise_keeper.common.UtilityBase
 import com.jc.promise_keeper.databinding.ActivitySplashBinding
 
 class SplashActivity : UtilityBase.BaseAppCompatActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
     override fun ActivitySplashBinding.onCreate() {
-        getHandler(MainActivity::class.java, 2500, true)
+        getHandler(SignUpActivity::class.java, 2500, true)
     }
 
 }
