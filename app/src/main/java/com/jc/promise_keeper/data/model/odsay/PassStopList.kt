ce3eab2c1f2509e9f7b015969de6d4a2f3b338be
jaecheon <jaecheon0222@gmail.com>
2022-03-27 19:22:40 +0900

    #33 필요 없는 데이터 삭제

--- app/src/main/java/com/jc/promise_keeper/data/model/odsay/PassStopList.kt
+++
@@ -1,9 +0,0 @@
-package com.jc.promise_keeper.data.model.odsay
-
-
-import com.google.gson.annotations.SerializedName
-
-data class PassStopList(
-    @SerializedName("stations")
-    val stations: List<Station>?
-)
