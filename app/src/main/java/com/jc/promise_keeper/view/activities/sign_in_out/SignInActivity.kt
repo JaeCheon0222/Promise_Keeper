338c0f481b9e753dd72a255fa61b7bcf04d46b16
jaecheon <jaecheon0222@gmail.com>
2022-03-28 17:09:57 +0900

    #34 페이스북 로그인

--- app/src/main/java/com/jc/promise_keeper/view/activities/sign_in_out/SignInActivity.kt
+++ app/src/main/java/com/jc/promise_keeper/view/activities/sign_in_out/SignInActivity.kt
@@ -1,6 +1,11 @@
 package com.jc.promise_keeper.view.activities.sign_in_out
 
+import android.content.Intent
 import android.util.Log
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.jc.promise_keeper.MainActivity
 import com.jc.promise_keeper.R
 import com.jc.promise_keeper.common.api.repository.UserRepository
@@ -10,6 +15,11 @@ import com.jc.promise_keeper.databinding.ActivitySignInBinding
 import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.*
 
 class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.activity_sign_in) {
 
@@ -18,6 +28,8 @@ class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.act
     private lateinit var email: String
     private lateinit var pw: String
 
+    lateinit var mCallbackManager: CallbackManager
+
     override fun ActivitySignInBinding.onCreate() {
         setEvents()
         initViews()
@@ -25,6 +37,7 @@ class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.act
 
     override fun initViews() {
         super.initViews()
+        mCallbackManager = CallbackManager.Factory.create()
         binding.autoLoginCheckBox.isChecked = Preferences.getAutoLogin(mContext)
 
     }
@@ -54,6 +67,59 @@ class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.act
                         postKakaoUserInfo()
                     }
                 }
+            }
+
+            facebookLogin.setOnClickListener {
+
+                LoginManager.getInstance().registerCallback(mCallbackManager, object: FacebookCallback<LoginResult> {
+                    override fun onCancel() {
+
+                    }
+
+                    override fun onError(error: FacebookException) {
+
+                    }
+
+                    override fun onSuccess(result: LoginResult) {
+
+                        // 1. 정보를 받아오면 뭘 할건지? 인터페이스 설정
+                        val graphRequest = GraphRequest.newMeRequest(result?.accessToken, object: GraphRequest.GraphJSONObjectCallback {
+                            override fun onCompleted(jsonObj: JSONObject?, response: GraphResponse?) {
+
+                                Log.d("받아온정보", jsonObj!!.toString())
+
+                                scope.launch {
+                                    val result = UserRepository.postSocialLogin("facebook", jsonObj.getString("id"), jsonObj.getString("name"))
+
+                                    if (result.isSuccessful) {
+
+                                        val body = result.body()!!
+
+                                        Preferences.setUserToken(mContext, body.data?.token!!)
+                                        showToast("${body.data.user?.nickName}님, 페북 로그인을 환영합니다!")
+
+                                        goToActivityIsFinish(MainActivity::class.java)
+                                        finish()
+
+                                    } else {
+                                        showToast("실패")
+                                    }
+                                }
+                            }
+
+                        })
+
+                        // 2. 실제로 요청 호출
+                        graphRequest.executeAsync()
+
+                    }
+
+                })
+
+                // 2. 실제로 페북 로그인 실행
+                // 공개 프로필 / 이메일 주소를 받아와 달라
+                LoginManager.getInstance().logInWithReadPermissions(this@SignInActivity, Arrays.asList("public_profile", "email"))
+
             }
 
 
@@ -96,7 +162,7 @@ class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.act
                         Preferences.setUserToken(mContext, body?.data?.token!!)
                         goToActivityIsFinish(MainActivity::class.java)
                         showToast("${body.data.user?.nickName}님, 카톡 로그인을 환영합니다!")
-
+                        finish()
                     } else {
                         showToast("정보 전달 실패")
                     }
@@ -128,6 +194,11 @@ class SignInActivity : BaseAppCompatActivity<ActivitySignInBinding>(R.layout.act
 
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 
